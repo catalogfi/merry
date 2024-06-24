69d6a288e864a1a7ed20fe042fb6aee687a0aae7
susruth <susruth@hashira.io>
2024-06-24 17:05:41 +0530

    add evm faucet

--- cmd/merry/faucet.go
+++ cmd/merry/faucet.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"math/big"
 	"net/http"
 
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/catalogfi/blockchain/localnet"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/cobra"
 )
 
@@ -23,35 +30,80 @@ func Faucet(state *State) *cobra.Command {
 				return fmt.Errorf("merry is not running")
 			}
 
-			payload, err := json.Marshal(map[string]string{
-				"address": to,
-			})
+			_, err := btcutil.DecodeAddress(to, &chaincfg.RegressionNetParams)
 			if err != nil {
-				return fmt.Errorf("failed to marshal address: %v", err)
+				if len(to) == 42 {
+					to = to[2:]
+				}
+				if len(to) == 40 {
+					_, err := hex.DecodeString(to)
+					if err != nil {
+						return fmt.Errorf("to is not an ethereum or a bitcoin regtest address: %s", to)
+					}
+					return fundEVM(to)
+				}
+				return fmt.Errorf("to is not an ethereum or a bitcoin regtest address: %s", to)
 			}
 
-			res, err := http.Post("http://127.0.0.1:3000/faucet", "application/json", bytes.NewBuffer(payload))
-			if err != nil {
-				return fmt.Errorf("failed to get funds from faucet: %v", err)
-			}
-			data, err := io.ReadAll(res.Body)
-			if err != nil {
-				return err
-			}
-			if res.StatusCode != http.StatusOK {
-				return errors.New(string(data))
-			}
-			var dat map[string]string
-			if err := json.Unmarshal([]byte(data), &dat); err != nil {
-				return errors.New("internal error, please try again")
-			}
-			if dat["txId"] == "" {
-				return errors.New("not successful")
-			}
-			fmt.Println("Successfully submitted at http://localhost:5000/tx/" + dat["txId"])
-			return nil
+			return fundBTC(to)
 		},
 	}
 	cmd.Flags().StringVar(&to, "to", "", "user should pass the address they needs to be funded")
 	return cmd
+}
+
+func fundEVM(to string) error {
+	ethAmount, _ := new(big.Int).SetString("1000000000000000000", 10)
+	wbtcAmount, _ := new(big.Int).SetString("100000000", 10)
+	tx, err := localnet.EVMWallet().Send(context.Background(), localnet.ETH(), common.HexToAddress(to), ethAmount)
+	if err != nil {
+		return fmt.Errorf("failed to send eth: %v", err)
+	}
+	fmt.Printf("Successfully sent %v ETH on Ethereum Localnet at: %s\n", ethAmount, tx.Hash().Hex())
+	tx2, err := localnet.EVMWallet().Send(context.Background(), localnet.WBTC(), common.HexToAddress(to), wbtcAmount)
+	if err != nil {
+		return fmt.Errorf("failed to send eth: %v", err)
+	}
+	fmt.Printf("Successfully sent %v WBTC on Ethereum Localnet at: %s\n", wbtcAmount, tx2.Hash().Hex())
+	tx3, err := localnet.EVMWallet().Send(context.Background(), localnet.ArbitrumETH(), common.HexToAddress(to), ethAmount)
+	if err != nil {
+		return fmt.Errorf("failed to send eth: %v", err)
+	}
+	fmt.Printf("Successfully sent %v ETH on Arbitrum Localnet at: %s\n", wbtcAmount, tx3.Hash().Hex())
+	tx4, err := localnet.EVMWallet().Send(context.Background(), localnet.ArbitrumWBTC(), common.HexToAddress(to), wbtcAmount)
+	if err != nil {
+		return fmt.Errorf("failed to send eth: %v", err)
+	}
+	fmt.Printf("Successfully sent %v WBTC on Arbitrum Localnet at: %s\n", wbtcAmount, tx4.Hash().Hex())
+	return nil
+}
+
+func fundBTC(to string) error {
+	payload, err := json.Marshal(map[string]string{
+		"address": to,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to marshal address: %v", err)
+	}
+
+	res, err := http.Post("http://127.0.0.1:3000/faucet", "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		return fmt.Errorf("failed to get funds from faucet: %v", err)
+	}
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return errors.New(string(data))
+	}
+	var dat map[string]string
+	if err := json.Unmarshal([]byte(data), &dat); err != nil {
+		return errors.New("internal error, please try again")
+	}
+	if dat["txId"] == "" {
+		return errors.New("not successful")
+	}
+	fmt.Println("Successfully submitted at http://localhost:5000/tx/" + dat["txId"])
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,13 +1,77 @@
 module github.com/catalogfi/merry
 
-go 1.16
+go 1.21
+
+toolchain go1.22.0
 
 require (
+	github.com/btcsuite/btcd v0.24.0
+	github.com/btcsuite/btcd/btcutil v1.1.5
+	github.com/catalogfi/blockchain v0.1.1
 	github.com/compose-spec/compose-go v0.0.0-20210729195839-de56f4f0cb3c
 	github.com/logrusorgru/aurora v2.0.3+incompatible
-	github.com/spf13/cobra v0.0.3
-	github.com/stretchr/testify v1.8.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	github.com/spf13/cobra v1.5.0
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/aead/siphash v1.0.1 // indirect
+	github.com/bits-and-blooms/bitset v1.10.0 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.3 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
+	github.com/btcsuite/btcwallet v0.16.9 // indirect
+	github.com/btcsuite/btcwallet/wallet/txsizes v1.2.4 // indirect
+	github.com/btcsuite/btcwallet/walletdb v1.4.2 // indirect
+	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd // indirect
+	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792 // indirect
+	github.com/consensys/bavard v0.1.13 // indirect
+	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/distribution/distribution/v3 v3.0.0-20210316161203-a01c71e2477e // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
+	github.com/ethereum/go-ethereum v1.14.5 // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/holiman/uint256 v1.2.4 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/kkdai/bstream v1.0.0 // indirect
+	github.com/lightninglabs/neutrino/cache v1.1.2 // indirect
+	github.com/lightningnetwork/lnd/fn v1.0.5 // indirect
+	github.com/lightningnetwork/lnd/tlv v1.2.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/mmcloughlin/addchain v0.4.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/supranational/blst v0.3.11 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	rsc.io/tmplfunc v0.0.3 // indirect
 )
